4507ce46c0f3e515601225cdadc92b71b32ac51e
luttfimohamad <luttfimohamad@gmail.com>
2025-05-13 21:50:47 +0700

    Refactor MainHeader component: separate background into its own component and styles, remove unused styles, and optimize imports

---
+++ components/main-header/main-header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import logoImages from '../../public/images/logo.png';
+import classes from './main-header.module.css';
+import MainHeaderBackground from './main-header-background';
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImages} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+            <li>
+              <Link href="/meals/share">Share a Meal</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
