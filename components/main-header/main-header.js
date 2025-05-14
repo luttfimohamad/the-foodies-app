d870a50ab4597a9c1f89e050ee3dc67f2c46276f
luttfimohamad <luttfimohamad@gmail.com>
2025-05-14 12:31:53 +0700

    Add NavLink component for navigation and refactor MainHeader to use it

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import logoImages from '../../public/images/logo.png';
 import classes from './main-header.module.css';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
   return (
@@ -19,13 +20,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
-            </li>
-            <li>
-              <Link href="/meals/share">Share a Meal</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
