e5744a404afb9712343c29a82543e62c37192710
luttfimohamad <luttfimohamad@gmail.com>
2025-05-14 15:00:49 +0700

    Refactor MealsPage to use Suspense for loading meals and add loading styles; increase simulated fetch delay to 5 seconds

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import classes from './page.module.css';
 import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +25,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
